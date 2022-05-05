12e49e26066823565a93c4ac11752319d6f0161d
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2022-05-05 14:04:26 -0500

    Fixup script for running tests in directory refs #48

--- scripts/run_app_tests.py
+++ scripts/run_app_tests.py
@@ -12,9 +12,16 @@ else:
 # Search apps/ folder for installed apps to run tests with
 for app in os.listdir(apps_folder):
 
+    # Sometimes we only want to run tests for a single or so app
+    if (len(sys.argv) > 1 and app not in sys.argv):
+        continue
+
     # Run each apps's VTB test suite
     if (os.path.exists(apps_folder+'/'+app+'/run_tests')):
         print("Running tests for", app)
-        os.system(apps_folder+'/'+app+'/run_tests  --pedantic-checks -j 16 --spec-file ../.. -i tests -t --longest-jobs=20')
+        os.chdir(apps_folder+'/'+app)
+        os.system('./run_tests --pedantic-checks -j 16 --spec-file ../.. -i tests -t --longest-jobs=1')
+        print("Tests finished for", app, "\n\n\n")
+        os.system('cd ../../')
     else:
         print("App", app, "not installed. init/update/make it")
