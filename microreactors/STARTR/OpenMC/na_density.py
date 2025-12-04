6e1b07747f22629ef8bfba73b0b449202f5f0214
Travis Lange <Travis.Lange@inl.gov>
2025-12-04 12:55:40 -0700

    Changes to STARTR models to pass pre-check. Issue #721

--- microreactors/STARTR/OpenMC/na_density.py
+++ microreactors/STARTR/OpenMC/na_density.py
@@ -1,6 +1,6 @@
 def calc_na_density(temperature=294):
     """
-    Calculate the density (g/cc) of Na coolant from O.J. Foust, Sodium-NaK Engineering Handbook, Gordin and 
+    Calculate the density (g/cc) of Na coolant from O.J. Foust, Sodium-NaK Engineering Handbook, Gordin and
     Breach Science Publishers Inc. (1972), eq. 1.3, p.g. 13
     """
     avail_temps = [250, 294, 600, 900, 1200, 2500] # cross-section temps in endfb 8.0 library
